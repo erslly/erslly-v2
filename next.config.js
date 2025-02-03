f09741fe9752a65f8dd30895c7a4599018bf8f36
Erdem <157350349+erslly@users.noreply.github.com>
2025-02-03 22:24:35 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,6 +13,16 @@ module.exports = {
         destination: 'https://x.com/ersllydev',
         permanent: true,
       },
+      {
+        source: '/instagram',
+        destination: 'https://instagram.com/erdemozbebek_',
+        permanent: true,
+      },
+      {
+        source: '/discord',
+        destination: 'https://discord.com/users/815668704435896321',
+        permanent: true,
+      },
     ]
   },
 }
