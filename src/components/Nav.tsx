a1e630fea9a061214d8b293752d9bca7a40f8ee2
Erdem <157350349+erslly@users.noreply.github.com>
2025-02-03 22:25:11 +0300

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -153,11 +153,11 @@ const Nav = () => {
 
                             <div className="flex flex-row items-center justify-center gap-6 py-4">
                                 <LinkButton
-                                    href={"https://github.com/erslly"}
+                                    href={"https://erslly.xyz/github"}
                                     icon={<SiGithub className="w-6 h-6 cursor-pointer" />}
                                 />
                                 <LinkButton
-                                    href={"https://x.com/ersllydev"}
+                                    href={"https://erslly.xyz/x"}
                                     icon={<SiX className="w-6 h-6 cursor-pointer" />}
                                 />
                                 <LinkButton
