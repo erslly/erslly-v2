7b8bde081979fa5a797ee9ad9d44b336c95759a9
Erdem <157350349+erslly@users.noreply.github.com>
2025-02-18 20:38:22 +0300

    Update skills.tsx

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,6 +1,5 @@
 import { motion } from 'framer-motion';
 
-// SkillItem bileşeni
 const SkillItem = ({ name, icon, href }: { name: string; icon: string; href: string }) => (
   <motion.a
     className="group relative flex items-center justify-center w-full h-36 sm:h-32 bg-primary border border-indigo-900 rounded-lg overflow-hidden transition-all cursor-pointer hover:scale-105 hover:bg-secondary active:scale-95"
@@ -8,7 +7,6 @@ const SkillItem = ({ name, icon, href }: { name: string; icon: string; href: str
     target="_blank"
     aria-label={name}
   >
-    {/* Görsel */}
     <div className="w-full h-full flex justify-center items-center">
       <img
         src={icon}
@@ -17,23 +15,19 @@ const SkillItem = ({ name, icon, href }: { name: string; icon: string; href: str
       />
     </div>
     
-    {/* Skill adı, imleç üzerine geldiğinde görünür */}
-    <p className="absolute bottom-3 left-1/2 transform -translate-x-1/2 text-sm font-medium opacity-0 group-hover:opacity-100 group-hover:translate-y-0 translate-y-10px transition-all text-white">
+    <p className="absolute bottom-3 left-3 text-sm font-medium opacity-0 group-hover:opacity-100 group-hover:translate-y-0 translate-y-10px transition-all text-white">
       {name}
     </p>
     
-    {/* Arka planda kararma efekti */}
-    <div className="absolute inset-0 bg-black/30 opacity-0 group-hover:opacity-100 transition-opacity duration-200 rounded-md"></div>
+    <div className="absolute inset-0 bg-black/30 opacity-0 group-hover:opacity-49 transition-opacity duration-200 rounded-md"></div>
   </motion.a>
 );
 
-// Skills bileşeni
 export default function Skills() {
-  // Teknolojilerin bilgileri (name, icon, href)
   const skills = [
     {
       name: 'TypeScript',
-      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1024px-Typescript_logo_2020.png',
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1024px-Typescript_logo_2020.svg.png',
       href: 'https://www.typescriptlang.org/'
     },
     {
@@ -43,12 +37,12 @@ export default function Skills() {
     },
     {
       name: 'React',
-      icon: 'https://cdn.worldvectorlogo.com/logos/react-1.svg',
+      icon: 'https://wallpaperaccess.com/full/3909258.jpg',
       href: 'https://reactjs.org/'
     },
     {
       name: 'Node.js',
-      icon: 'https://romanbaba.vercel.app/imgs/node.svg',
+      icon: 'https://i.pinimg.com/474x/f0/a7/aa/f0a7aa555f3deb6b34bd38c65eb421a5.jpg',
       href: 'https://nodejs.org/'
     },
     {
@@ -58,7 +52,7 @@ export default function Skills() {
     },
     {
         name: 'HTML5',
-        icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_3V0WmaHi7hLENYgmyF3iKWD7BP4bn82grA&s',
+        icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtQOLt94fHEMPKxkpqM1E8y9Jnz7clTa5JKQ&s',
         href: 'https://www.python.org/'
     },
     {
@@ -84,12 +78,11 @@ export default function Skills() {
   ];
 
   return (
-    <div className="flex flex-col justify-start items-center min-h-screen p-6 bg-background pt-60"> {/* pt-12 ile üst boşluğu artırdık */}
+    <div className="flex flex-col justify-start items-center min-h-screen p-6 bg-background pt-40 md:pt-60"> 
       <h2 className="text-3xl font-bold md:text-4xl mb-2 text-left w-full">Skills 🛠️</h2>
       <p className="text-sm font-light text-left w-full text-gray-500 mb-8">
       </p>
 
-      {/* Skill grid */}
       <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 gap-6 w-full">
         {skills.map((skill, index) => (
           <SkillItem key={index} name={skill.name} icon={skill.icon} href={skill.href} />
