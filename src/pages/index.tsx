82be84f80a4a9369fa5e21fd20268da26610b6c3
Erdem <157350349+erslly@users.noreply.github.com>
2025-02-19 17:13:16 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ interface LanyardData {
 }
 
 const Index = () => {
-    const [status, setStatus] = useState<string>('border-gray-500');
+    const [status, setStatus] = useState<string>('border-gray-500 bg-gray-500');
     const [loading, setLoading] = useState<boolean>(true);
     const [currentDate, setCurrentDate] = useState<string>('');
     const [spotifyTrack, setSpotifyTrack] = useState<string>(''); 
@@ -32,16 +32,15 @@ const Index = () => {
                 const userStatus = data.data.discord_status;
 
                 if (userStatus === 'online') {
-                    setStatus('border-green-500');
+                    setStatus('border-green-500 bg-green-500');
                 } else if (userStatus === 'idle') {
-                    setStatus('border-yellow-500');
+                    setStatus('border-yellow-500 bg-yellow-500');
                 } else if (userStatus === 'dnd') {
-                    setStatus('border-red-500');
+                    setStatus('border-red-500 bg-red-500');
                 } else {
-                    setStatus('border-gray-500');
+                    setStatus('border-gray-500 bg-gray-500'); // Çevrimdışı için gri
                 }
 
-                // Spotify bilgilerini kontrol et
                 if (data.data.spotify) {
                     setSpotifyTrack(data.data.spotify.song);
                     setSpotifyArtist(data.data.spotify.artist);
@@ -55,7 +54,7 @@ const Index = () => {
                 }
             } catch (error) {
                 console.error("Lanyard API hatası:", error);
-                setStatus('border-gray-500');
+                setStatus('border-gray-500 bg-gray-500');
             } finally {
                 setLoading(false);
             }
@@ -76,15 +75,11 @@ const Index = () => {
             setCurrentDate(now.toLocaleString('en-US', options));
         };
 
-        const updateSpotifyStatus = () => {
-            fetchUserStatus(); 
-        };
-
         setInterval(updateDateTime, 1000);
-        setInterval(updateSpotifyStatus, 5000); 
+        setInterval(fetchUserStatus, 5000);
 
         fetchUserStatus();
-        const interval = setInterval(fetchUserStatus, 5000); 
+        const interval = setInterval(fetchUserStatus, 5000);
 
         return () => {
             clearInterval(interval);
@@ -97,7 +92,7 @@ const Index = () => {
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.95 }}
             transition={{ ease: "easeOut", duration: 0.15 }}
-            className="h-screen flex flex-col justify-start items-start text-white overflow-hidden px-8 py-40 md:py-60" 
+            className="h-screen flex flex-col justify-start items-start text-white overflow-hidden px-8 py-40 md:py-60"
         >
     
             <div className="flex flex-col items-start text-left">
@@ -125,69 +120,68 @@ const Index = () => {
             </div>
 
             <div className="mt-4 flex flex-col items-start space-y-2 text-gray-300 text-sm">
-            {currentDate ? (
-                <span>{currentDate}</span>
+                {currentDate ? (
+                    <span>{currentDate}</span>
+                ) : (
+                    <div className="flex items-center justify-center">
+                        <div className="w-4 h-4 border-2 border-purple-500 border-t-transparent rounded-full animate-spin"></div>
+                    </div>
+                )}
+                <div className="w-full border-t border-gray-600 my-2"></div>
+            </div>
+
+            <div className="mt-4 relative flex items-center">
+                <div className="relative">
+                    <img
+                        src="https://p.erslly.xyz/ersllydev.jpg"
+                        alt="Avatar"
+                        className={`w-24 h-24 rounded-full border-4 ${status.split(" ")[0]}`}
+                    />
+                    {loading ? (
+                        <span className="absolute bottom-0 right-0 w-5 h-5 bg-gray-300 rounded-full border-2 border-gray-900"></span>
+                    ) : (
+                        <span className={`absolute bottom-0 right-0 w-5 h-5 ${status.split(" ")[1]} rounded-full border-2 border-gray-900`}></span>
+                    )}
+                </div>
+                <div className="ml-4">
+                    <p className="text-2xl font-bold">Erdem</p>
+                    <p className="text-sm font-light">Erslly</p>
+                </div>
+            </div>
+
+            {spotifyTrack && spotifyCover ? (
+                <div className="mt-10 flex items-center space-x-6">
+                    <img src={spotifyCover} alt="Spotify Cover" className="w-24 h-30 rounded-lg" />
+                    <div className="flex flex-col">
+                        <a
+                            href={`https://open.spotify.com/track/${spotifyTrackId}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-white font-semibold text-lg hover:underline"
+                        >
+                            {spotifyTrack}
+                        </a>
+                        <a
+                            href={`https://open.spotify.com/search/${encodeURIComponent(spotifyArtist)}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-gray-400 text-sm hover:underline"
+                        >
+                            {spotifyArtist}
+                        </a>    
+                        <span className="text-gray-500 text-xs">🎵 Now Playing on Spotify</span>
+                    </div>
+                </div>
             ) : (
-                <div className="flex items-center justify-center">
-                    <div className="w-4 h-4 border-2 border-purple-500 border-t-transparent rounded-full animate-spin"></div>
+                <div className="mt-4 flex items-center space-x-4">
+                    <FaSpotify className="text-gray-400 text-2xl" />
+                    <div className="flex flex-col">
+                        <p className="text-sm font-light text-gray-400">Not Listening to Anything</p>
+                    </div>
                 </div>
             )}
-            <div className="w-full border-t border-gray-600 my-2"></div>
-        </div>
-
-        <div className="mt-4 relative flex items-center">
-            <div className="relative">
-                <img
-                src="https://p.erslly.xyz/ersllydev.jpg"
-                alt="Avatar"
-                className={`w-24 h-24 rounded-full border-4 ${status}`}
-                />
-                {loading ? (
-                <span className="absolute bottom-0 right-0 w-5 h-5 bg-gray-300 rounded-full border-2 border-gray-900"></span>
-                ) : (
-                <span
-                    className={`absolute bottom-0 right-0 w-5 h-5 ${status === 'border-red-500' ? 'bg-red-500' : status === 'border-yellow-500' ? 'bg-yellow-500' : 'bg-green-500'} rounded-full border-2 border-gray-900`}></span>
-                )}
-            </div>
-            <div className="ml-4">
-                <p className="text-2xl font-bold">Erdem</p>
-                <p className="text-sm font-light">Erslly</p>
-            </div>
-        </div>
-
-        {spotifyTrack && spotifyCover ? (
-            <div className="mt-10 flex items-center space-x-6">
-                <img src={spotifyCover} alt="Spotify Cover" className="w-24 h-30 rounded-lg" />
-                <div className="flex flex-col">
-                    <a
-                        href={`https://open.spotify.com/track/${spotifyTrackId}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-white font-semibold text-lg hover:underline"
-                    >
-                        {spotifyTrack}
-                    </a>
-                    <a
-                        href={`https://open.spotify.com/search/${encodeURIComponent(spotifyArtist)}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-gray-400 text-sm hover:underline"
-                    >
-                        {spotifyArtist}
-                    </a>
-                    <span className="text-gray-500 text-xs">🎵 Now Playing on Spotify</span>
-                </div>
-            </div>
-        ) : (
-            <div className="mt-4 flex items-center space-x-4">
-                <FaSpotify className="text-gray-400 text-2xl" />
-                <div className="flex flex-col">
-                    <p className="text-sm font-light text-gray-400">Not Listening to Anything</p>
-                </div>
-            </div>
-        )}
-    </motion.div>
-  );
+        </motion.div>
+    );
 };
 
 export default Index;
