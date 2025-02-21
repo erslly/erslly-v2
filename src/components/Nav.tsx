9908f06913a7b52d7bb72a59d7dfe0d616a038dd
Erdem <157350349+erslly@users.noreply.github.com>
2025-02-21 19:15:13 +0300

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,11 +2,17 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const LandingButton = ({ name, link, selected }) => {
+interface LandingButtonProps {
+  name: string;
+  link: string;
+  selected: boolean;
+}
+
+const LandingButton: React.FC<LandingButtonProps> = ({ name, link, selected }) => {
   return (
     <Link href={link} legacyBehavior>
       <a
-        className={`px-1 py-40   text-base font-semibold rounded-md transition-all duration-75 ${
+        className={`px-1 py-40 text-base font-semibold rounded-md transition-all duration-75 ${
           selected ? "text-white" : "text-gray-400 hover:text-white"
         }`}
       >
@@ -21,10 +27,10 @@ const Nav = () => {
 
   return (
     <motion.div
-      className="fixed top-[-2rem] left-1/2 transform -translate-x-1/2 z-[999] flex gap-6 bg-transparent sm:top-0" 
+      className="fixed top-[-2rem] left-1/2 transform -translate-x-1/2 z-[999] flex gap-6 bg-transparent sm:top-0"
     >
       <LandingButton name="Home" link="/" selected={router.pathname === "/"} />
-      <LandingButton name="Projects" link="/project" selected={router.pathname === "/projects"} />
+      <LandingButton name="Projects" link="/project" selected={router.pathname === "/project"} />
       <LandingButton name="Skills" link="/skills" selected={router.pathname === "/skills"} />
     </motion.div>
   );
