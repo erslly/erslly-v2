8ca8f1a30e53421a82545defa6078e86f28772fe
Erdem <157350349+erslly@users.noreply.github.com>
2025-02-23 16:27:23 +0300

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 interface LandingButtonProps {
   name: string;
@@ -24,10 +25,25 @@ const LandingButton: React.FC<LandingButtonProps> = ({ name, link, selected }) =
 
 const Nav = () => {
   const router = useRouter();
+  const [isNavbarVisible, setIsNavbarVisible] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsNavbarVisible(false); 
+      } else {
+        setIsNavbarVisible(true); 
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <motion.div
       className="fixed top-[-2rem] left-1/2 transform -translate-x-1/2 z-[999] flex gap-6 bg-transparent sm:top-0"
+      style={{ opacity: isNavbarVisible ? 1 : 0, transition: "opacity 0.3s ease" }} 
     >
       <LandingButton name="Home" link="/" selected={router.pathname === "/"} />
       <LandingButton name="Projects" link="/project" selected={router.pathname === "/project"} />
